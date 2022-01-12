7ce4688f961fdb79ab8ac16cc2282b715eec15a0
Alena <72409651+AlenaKhmyz@users.noreply.github.com>
2022-01-13 01:23:41 +0300

    added gallery

---
+++ js/index.js
@@ -0,0 +1,23 @@
+async function getCards() {
+  let response = await fetch('https://jsonplaceholder.typicode.com/photos')
+  let cards = await response.json()
+  cards = cards.splice(0, 11)
+
+  let list = document.querySelector('.cards')
+ 
+  let key
+  for (key in cards) {
+    list.innerHTML += `
+      <div class="card-date">
+        <h3 class="card-name">${cards[key].title}</h3>
+        <img src="${cards[key].url}" width="400">
+      </div>
+    `
+      
+    
+    console.log(cards[key])
+  }
+
+}
+
+getCards()
